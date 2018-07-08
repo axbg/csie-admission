85e0d70dad28a3bec0f4db81755c76e07722bf84
Alex Bisag <bisagalexstefan@gmail.com>
2018-07-08 17:32:48 +0300

    Worked on index page progresiveness

--- admitere-front/js/index.js
+++ admitere-front/js/index.js
@@ -11,7 +11,7 @@ let upload = function(){
 
     socket.on('update', (data) => {
       document.getElementById('nr').innerHTML = '<h1 class="counter">' + data[2] + '</h1>';
-     document.getElementById('hour').innerHTML = '<h3>Următoarea serie de 10 persoane a intrat la ora ' + data[1] + '</h1>';
+     document.getElementById('hour').innerHTML = '<h3>Ultima serie de 10 persoane a intrat la ora ' + data[1] + '</h1>';
 	document.getElementById('avg').innerHTML = 'Media de așteptare pentru intrarea unei serii noi este ' + data[0] + ' minute.';
     });
 
