c76c82fa7c4a10e5012c4024a25e0acfb4c1c244
Alex Bisag <bisagalexstefan@gmail.com>
2018-07-10 01:23:32 +0300

    Removed empty routes

--- admitere-back/routes/contestant.js
+++ admitere-back/routes/contestant.js
@@ -3,8 +3,6 @@ let router = express.Router();
 let contestantsController = require('../public/controllers/contestantsController');
 let usersController = require('../public/controllers/usersController');
 
-router.get('/', contestantsController.getContestants);
 router.post('/addContestant', contestantsController.addContestants);
-router.post('/deleteContestant', usersController.authMiddleware, contestantsController.deleteContestants);
 
 module.exports = router;
