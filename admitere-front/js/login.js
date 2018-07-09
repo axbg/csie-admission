3759e3397c46f4f515c38ea19be56557af6482f6
Alex Bisag <bisagalexstefan@gmail.com>
2018-07-09 20:59:11 +0300

    Edy MVP

--- admitere-front/js/login.js
+++ admitere-front/js/login.js
@@ -1,33 +1,33 @@
-window.onload = function(){
+window.onload = function () {
 
   document.getElementById("username").focus();
 
 };
 
-let login = function(){
+let login = function () {
 
   let username = document.getElementById("username");
   let password = document.getElementById("password");
 
-  if(username.value !== "" || password.value !== ""){
+  if (username.value !== "" || password.value !== "") {
+
     let params = new URLSearchParams();
     params.append('username', username.value);
     params.append('password', password.value);
 
-
-    axios.post("http://localhost:3000/user/login", params, {withCredentials: true})
+    axios.post("http://admitere.sisc.ro/user/login", params,
+        {withCredentials: true})
     .then((result) => {
       console.log(result);
       //window.location.replace("update.html");
     }).catch((ex) => {
       alert("Credentials are not valid!");
       username.value = "";
-      password.value =  "";
+      password.value = "";
       username.focus();
     });
-  } else{
+  } else {
     alert("Insert credentials");
   }
-
 
 };
